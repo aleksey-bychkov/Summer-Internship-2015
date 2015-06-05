26db2458615196c453fbce9e3d3b48b70febe603
Yyy4401 <aleksey.bychkov@gmail.com>
2015-06-05 17:06:56 -0400

    Made task work till infoWindow crashes site

--- Task 5/ScriptTask5.js
+++ Task 5/ScriptTask5.js
@@ -9,113 +9,108 @@ Eastbanc.Internship.Task5 = function()
 {
     var that = this;
     that.map = new google.maps.Map(document.getElementById('map-canvas'),
-        {
-            center: new google.maps.LatLng(38.9055751, -77.0611825),
-            zoom: 15
-        });
+    {
+        center: new google.maps.LatLng(38.9055751, -77.0611825),
+        zoom: 15
+    });
 
-    function placeMarkers()
+    function placeMarker()
     {
         var url = document.getElementById("dataURL").value;
-        var data = getData(url);
-        var markers = [];
 
-
-        for(var x = 0; x < data.length; x++)
+        $.ajax(
         {
-            (function makeMarkers()
+            url: url,
+            jsonp: "callback",
+            dataType: "jsonp",
+            success: function(data)
             {
-                var current = data[x];
-                var latlng = getLatLng(current);
+                place(data);
+            },
+            error: function ()
+            {
+                alert("Failed to retrieve info!");
+            }
+        });
 
-                //makes the markers
-                var marker = new google.maps.Marker
-                (
-                    {
-                        map: that.map,
-                        title: x,
-                        position: latlng
-                    }
-                );
+       function place(information)
+       {
+           var markers = [];
 
 
-                google.maps.event.addListener(marker, 'click', function makeWindows() {
-                    //closes window if one exists
-                    if (window)
-                        window.close();
+           for(var x = 0; x < information.length; x++)
+           {
+               (function makeMarkers()
+               {
+                   var current = information[x];
+                   var latlng = new google.maps.LatLng(current.Lat, current.Lon);
 
-                    //makes and opens a new window for the current marker
-                    window = new google.maps.InfoWindow
-                    (
-                        {
-                            content: info = '<div id="content">' +
-                                '<div id="siteNotice">' +
-                                '</div>' +
-                                '<h1 id="firstHeading" class="firstHeading"></h1>' +
-                                '<div id="bodyContent">' +
-                                '<table>' +
-                                '<tr>' +
-                                '<th>Agency Id</th><td>' + current.AgencyId + '</td>' +
-                                '</tr>' +
-                                '<tr>' +
-                                '<th>Device Id</th><td>' + current.DeviceId + '</td>' +
-                                '</tr>' +
-                                '<tr>' +
-                                '<th>Is Standing</th><td>' + current.IsStanding + '</td>' +
-                                '</tr>' +
-                                '<tr>' +
-                                '<th>Location</th><td>' + latlng.toString() + '</td>' +
-                                '</tr>' +
-                                '<tr>' +
-                                '<th>Date</th><td>' + current.Date + '</td>' +
-                                '</tr>' +
-                                '<tr>' +
-                                '<th>Vehicle Id</th><td>' + current.VehicleId + '</td>' +
-                                '</tr>' +
-                                '</table>' +
-                                '</div>' +
-                                '</div>'
-                        }
-                    );
+                   //makes the markers
+                   var marker = new google.maps.Marker
+                   (
+                       {
+                           map: that.map,
+                           title: x+"",
+                           position: latlng
+                       }
+                   );
 
-                    //binds the window to the marker&map
-                    window.open(that.map, marker);
-                });
+                   google.maps.event.addListener(marker, 'click', function makeWindows()
+                   {
+                       //closes window if one exists
+                       if (window)
+                           window.close();
 
-                //adds the markers
-                markers.push(marker);
-            })();
-        }
+                       //makes and opens a new window for the current marker
+                       window = new google.maps.InfoWindow
+                       (
+                           {
+                               content: info = '<div id="content">' +
+                                   '<div id="siteNotice">' +
+                                   '</div>' +
+                                   '<h1 id="firstHeading" class="firstHeading"></h1>' +
+                                   '<div id="bodyContent">' +
+                                   '<table>' +
+                                   '<tr>' +
+                                   '<th>Agency Id</th><td>' + current.AgencyId + '</td>' +
+                                   '</tr>' +
+                                   '<tr>' +
+                                   '<th>Device Id</th><td>' + current.DeviceId + '</td>' +
+                                   '</tr>' +
+                                   '<tr>' +
+                                   '<th>Is Standing</th><td>' + current.IsStanding + '</td>' +
+                                   '</tr>' +
+                                   '<tr>' +
+                                   '<th>Location</th><td>' + latlng.toString() + '</td>' +
+                                   '</tr>' +
+                                   '<tr>' +
+                                   '<th>Date</th><td>' + current.Date + '</td>' +
+                                   '</tr>' +
+                                   '<tr>' +
+                                   '<th>Vehicle Id</th><td>' + current.VehicleId + '</td>' +
+                                   '</tr>' +
+                                   '</table>' +
+                                   '</div>' +
+                                   '</div>'
+                           }
+                       );
 
-        function getData(url)
-        {
-            $.ajax(
-                {
-                    url: url,
-                    jsonp: "callback",
-                    dataType: "jsonp",
-                    success: function(data)
-                    {
-                        return data;
-                    },
-                    error: function ()
-                    {
-                        alert("Failed to retrieve data!");
-                    }
-                });
-        }
+                       //binds the window to the marker&map
+                       window.open(that.map, marker);
+                   });
 
-        function getLatLng(data)
-        {
-            return new google.maps.LatLng(data.lat, data.lon);
-        }
+                   //adds the markers
+                   markers.push(marker);
+               })();
+           }
+
+       }
 
     }
 
     // constructor
-    return
-    {
-        placeMarkers: placeMarkers
+    return{
+        placeMarkers: placeMarker
     };
 
 };
